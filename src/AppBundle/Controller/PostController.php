6f949beeb3f2e9d3450951b15b2e73db61b338b5
carlmarkegang <carl.markegang@linasmatkasse.se>
2018-01-22 19:00:49 +0100

    subpost reply

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -21,33 +21,32 @@ class PostController extends Controller
     {
         $session = new Session();
         $postClass = new Post();
-        $users = new Users();
         $formView = array();
-
         $repository = $this->getDoctrine()->getRepository(Post::class);
-
         $userRepository = $this->getDoctrine()->getRepository(Users::class);
+        $em = $this->getDoctrine()->getManager();
 
-        if($session->get('username') && $session->get('id')){
+        if ($session->get('username') && $session->get('id')) {
             $form = $this->createForm(PostType::class, $postClass);
             $formView = $form->createView();
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
 
-                $postClass->setUsername( $userRepository->findoneBy(array('id' => $session->get('id'))) );
                 $postClass->setUserId($session->get('id'));
                 $postClass->setCreated(time());
-                $em = $this->getDoctrine()->getManager();
+                $postClass->setUsername($userRepository->findoneBy(array('id' => $session->get('id'))));
+
+                if ($id) {
+                    $postClass->setType('sub');
+                    $postClass->setParent($id);
+                }
+
                 $em->persist($postClass);
                 $em->flush();
             }
         }
 
-        if (!$id){
-            $post = $repository->findBy(array('type' => 'main'));
-        }
-
-        if (!$post) {
+        if ($id) {
             $post = $repository->find($id);
             $subpost = $repository->findBy(array('type' => 'sub', 'parent' => $id));
             $post = array($post);
@@ -58,6 +57,7 @@ class PostController extends Controller
             ));
         }
 
+        $post = $repository->findBy(array('type' => 'main'));
         return $this->render('posts.html.twig', array(
             'posts' => (array)$post,
             'subposts' => array(''),
